a4f8f3fc4cfb00eb4aa7ea5011e1b9b883334e80
A_Random_Guy <dandanny5157@gmail.com>
2025-06-04 00:42:52 +0800

    Overhauled FlatNaming and StructuredNaming

--- DNSSim/FlatNamingService.java
+++ DNSSim/FlatNamingService.java
@@ -2,19 +2,52 @@ package DNSSim;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class FlatNamingService {
-    private Map<String, String> nameToIpMap;
+    private Map<String, Resource> flatRegistery; //Map to store the resource name and the resource object
 
     public FlatNamingService() {
-        this.nameToIpMap = new HashMap<>();
+        this.flatRegistery = new HashMap<>();
     }
 
-    public void register(String name, String ip) {
-        nameToIpMap.put(name, ip);
+    public boolean register(String name, Resource resource) {
+        if (flatRegistery.containsKey(name)) {
+            throw new IllegalArgumentException("Resource already registered");
+        } else {
+            flatRegistery.put(name, resource);
+            System.out.println("Registered: " + name + " -> " + resource.getLocation());
+            return true;
+        }
     }
 
-    public String lookup(String name) {
-        return nameToIpMap.get(name);
+    public boolean unregister(String name) {
+        if (flatRegistery.containsKey(name)) {
+            flatRegistery.remove(name);
+            System.out.println("Unregistered: " + name);
+            return true;
+        } else {
+            System.out.println("Unregistration Failed: " + name);
+            return false;
+        }
+    }
+
+    /*
+     * lookup method to lookup a resource by name
+     * returns an optional string of the resource location
+     * if the resource is not found, returns an empty optional
+     * Dev note: I just learned about Optional. Apparently it's something that can be used to avoid null being returned.
+     * https://www.baeldung.com/java-optional
+     */
+
+    public Optional<String> lookup(String name) {
+        Resource resource = flatRegistery.get(name);
+        if (resource == null) {
+            System.out.println("Name Lookup Failed: " + name);
+            return Optional.empty();
+        } else {
+            System.out.println("Name Lookup Successful: " + name + " -> " + resource.getLocation());
+            return Optional.of(resource.getLocation());
+        }
     }
 }
--- DNSSim/StructuredNamingService.java
+++ DNSSim/StructuredNamingService.java
@@ -1,16 +1,50 @@
 package DNSSim;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.HashMap;
 
 public class StructuredNamingService {
-    private Map<String, String> nameToIpMap;
+    private Map<String, Resource> structuredRegistery;
 
     public StructuredNamingService() {
-        this.nameToIpMap = new HashMap<>();
+        this.structuredRegistery = new HashMap<>();
     }
 
-    public void register(String name, String ip) {
-        nameToIpMap.put(name, ip);
+    public boolean register(String path, Resource resource) {
+        if (path == null || path.trim().isEmpty() || !path.startsWith("/")) {
+            System.out.println("Error: Invalid Path: " + path);
+            return false;
+        }
+        if (structuredRegistery.containsKey(path)) {
+            System.out.println("Error: Path already registered: " + path);
+            return false;
+        } else {
+            structuredRegistery.put(path, resource);
+            System.out.println("Registered: " + path + " -> " + resource.getLocation());
+            return true;
+        }
+    }
+
+    public boolean unregister(String path) {
+        if (structuredRegistery.containsKey(path)) {
+            structuredRegistery.remove(path);
+            System.out.println("Unregistered: " + path);
+            return true;
+        } else {
+            System.out.println("Unregistration Failed: " + path);
+            return false;
+        }
+    }
+
+    public Optional<String> lookup(String path) {
+        Resource resource = structuredRegistery.get(path);
+        if (resource == null) {
+            System.out.println("Name Lookup Failed: " + path);
+            return Optional.empty();
+        } else {
+            System.out.println("Name Lookup Successful: " + path + " -> " + resource.getLocation());
+            return Optional.of(resource.getLocation());
+        }
     }
 }
